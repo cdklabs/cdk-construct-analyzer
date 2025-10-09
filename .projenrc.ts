6cc2d211c93c0c92e07338b2f00dac2cb148647d
Benny Huang <behuang@amazon.com>
2025-10-09 14:06:34 -0400

    created barebone cli that prints hello world

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,15 @@
-import { CdklabsTypeScriptProject } from "cdklabs-projen-project-types";
+import { CdklabsTypeScriptProject } from 'cdklabs-projen-project-types';
 const project = new CdklabsTypeScriptProject({
-  defaultReleaseBranch: "main",
-  devDeps: ["cdklabs-projen-project-types"],
-  name: "@cdklabs/cdk-construct-analyzer",
+  defaultReleaseBranch: 'main',
+  devDeps: ['cdklabs-projen-project-types'],
+  name: '@cdklabs/cdk-construct-analyzer',
   projenrcTs: true,
   release: false,
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: ['yargs'], /* Runtime dependencies of this module. */
+  bin: {
+    'cdk-construct-analyzer': './bin/cdk-construct-analyzer',
+  },
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
-  }
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+export function cli() {
+  void yargs(hideBin(process.argv))
+    .command(
+      '$0',
+      'CDK Construct Analyzer CLI',
+      {},
+      () => {
+        console.log('Hello World!');
+      },
+    )
+    .help()
+    .argv;
 }

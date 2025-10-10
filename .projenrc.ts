46e540a5da9d0993ce7caefe78d9c4b330adbf39
huang-benny <bennyhuang814@gmail.com>
2025-10-10 11:52:08 -0400

    feat: initial barebone cli (#2)
    * created barebone cli that prints hello world
    
    * fixed standard test
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * Addressed feedback, removed unnecessary files, and removed @cdklabs
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: Benny Huang <behuang@amazon.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,18 @@
-import { CdklabsTypeScriptProject } from "cdklabs-projen-project-types";
+import { CdklabsTypeScriptProject } from 'cdklabs-projen-project-types';
+import { NpmAccess } from 'projen/lib/javascript';
+
 const project = new CdklabsTypeScriptProject({
-  defaultReleaseBranch: "main",
-  devDeps: ["cdklabs-projen-project-types"],
-  name: "@cdklabs/cdk-construct-analyzer",
+  defaultReleaseBranch: 'main',
+  devDeps: ['cdklabs-projen-project-types', '@types/jest'],
+  name: 'cdk-construct-analyzer',
   projenrcTs: true,
   release: false,
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  jest: true,
+  deps: ['yargs'], /* Runtime dependencies of this module. */
+  bin: {
+    'cdk-construct-analyzer': './bin/cdk-construct-analyzer',
+  },
+  npmAccess: NpmAccess.PUBLIC,
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
